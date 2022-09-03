3dd1b35f8cbff25f580e5c7a5b5767fb54f2227f
morteza <mortezashoeibi77@gmail.com>
2022-09-03 14:36:18 +0430

    code the second program
    crypto_currency_price:
    	it's good to know your favourite crypto currencies price

---
+++ crypto_currency_price/main.py
@@ -0,0 +1,60 @@
+import flet
+from flet import Page, Text, TextField, Row, Column, ElevatedButton
+import requests
+
+
+def main(page: Page):
+    # page general settings
+    page.title = 'Crypto price'
+    page.vertical_alignment = 'start'
+
+    # page controls.
+    symbol = TextField(label="Enter the crypto symbol!", autofocus=True)
+    display_price = Column()
+
+    # checks the entered value.
+    def check_status(response):
+        if response.status_code == 200:
+            data = response.json()
+            display_price.controls.append(Text(value=f"The {data['symbol']} price is {data['price']}", size=30, color="white", bgcolor="green", weight="bold"))
+            
+        elif response.status_code == 403:
+            display_price.controls.append(Text(value=f"Turn on your VPN", size=30, color="white", bgcolor="pink"))
+            
+        else:
+            display_price.controls.append(Text(value=f"Couldn't find anything, are you sure about this '{symbol.value}' symbol?", size=30, color="white", bgcolor="pink"))
+
+    # button.
+    def button_clicked(e):
+        # upper casing the symbol.value
+        symbol.value = str(symbol.value).upper()
+        response = requests.get(f"https://api.binance.com/api/v3/ticker/price?symbol={symbol.value}")
+
+        # sends the response variable to be validated.
+        check_status(response)
+
+        # sets the symbol value to an empty string.
+        symbol.value = '' 
+        page.update()
+        symbol.focus()
+
+    # adds controls to the page.
+    page.add(
+        Row(
+            [
+                symbol,
+                ElevatedButton("check price!", on_click=button_clicked),
+            ],
+            
+            alignment="center",
+        ),
+
+        Column(
+            [
+                display_price
+            ],
+            alignment="center",
+            ))
+
+
+flet.app(port=8000, target=main)
